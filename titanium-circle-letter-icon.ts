92b10775b27f736e539115a5aa2f794d9bd5418e
wyatt Morrow <wyatt-morrow@leavitt.com>
2017-07-03 16:27:26 -0600

    moves logic to variable

--- titanium-circle-letter-icon.ts
+++ titanium-circle-letter-icon.ts
@@ -73,14 +73,15 @@ class TitaniumCircleLetterIcon extends Polymer.Element {
         colors[37] = "#283593";
 
         var firstChar = str.toUpperCase().substring(0, 1);
+        var firstCharLess55 = firstChar.charCodeAt(0) - 55;
         if (!isNaN(parseInt(firstChar))) {
             return colors[parseInt(firstChar)];
         }
-        else if ((firstChar.charCodeAt(0) - 55) < 0 || (firstChar.charCodeAt(0) - 55) > 37) {
+        else if (firstCharLess55 < 0 || firstCharLess55 > 37) {
             return colors[25];
         }
         else {
-            return colors[firstChar.charCodeAt(0) - 55];
+            return colors[firstCharLess55];
         }
     }
 }
