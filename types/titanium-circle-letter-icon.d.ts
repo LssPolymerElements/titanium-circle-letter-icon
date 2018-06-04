ec7fd12dc9f841aac34fc47db0e70a302b450cc7
Aaron Drabeck <github@aarondrabeck.com>
2018-06-04 10:36:42 -0600

    Converts to polymer decorators; adds clang; adds start-up path.

---
+++ types/titanium-circle-letter-icon.d.ts
@@ -0,0 +1,8 @@
+declare class TitaniumCircleLetterIcon extends Polymer.Element {
+    size: number;
+    text: string;
+    divideByTwo(size: any): number;
+    getFirstLetter(value: string): string;
+    getStyle(text: string, size: any): string;
+    stringToColor(str: any): any;
+}
